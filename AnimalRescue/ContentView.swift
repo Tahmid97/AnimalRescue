d556bea00d9b269f8ef5b72358c8c64abaef8354
Tahmid Muttaki <tahmid97@vt.edu>
2020-12-01 22:14:55 -0500

    Initial Commit

---
+++ AnimalRescue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimalRescue
+//
+//  Created by CS3714 on 12/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
