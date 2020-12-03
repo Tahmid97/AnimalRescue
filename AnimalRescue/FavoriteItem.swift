24d94b364f3c3b6369092d0b401676d277ab41a2
CS3714 <cs3714@h80adbfe5.dhcp.vt.edu>
2020-12-03 00:41:21 -0500

    changed list item for now

--- AnimalRescue/FavoriteItem.swift
+++ AnimalRescue/FavoriteItem.swift
@@ -2,7 +2,7 @@
 //  FavoritesItem.swift
 //  AnimalRescue
 //
-//  Created by CS3714 on 12/2/20.
+//  Created by Boshen Zhang on 12/2/20.
 //  Copyright © 2020 CS3714-91386 Team 16. All rights reserved.
 //
 
@@ -16,19 +16,14 @@ struct FavoriteItem: View {
     var body: some View {
         HStack {
             // Public function getImageFromUrl is given in UtilityFunctions.swift
-//            getImageFromUrl(url:"https://image.tmdb.org/t/p/w500/\(movie.posterFileName)", defaultFilename: "ImageUnavailable")
-//
-//            .resizable()
-//                .aspectRatio(contentMode: .fit)
-//                .frame(width: 80.0)
+            getImageFromUrl(url:"\(animal.photoUrl)", defaultFilename: "ImageUnavailable")
+
+            .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 80.0)
            
             VStack(alignment: .leading) {
                 Text(animal.animalType)
-                HStack {
-
-                    Text("\(animal.animalType)")
-                }
-                Text("\(animal.animalType)")
              
             }
             // Set font and size for the whole VStack content
