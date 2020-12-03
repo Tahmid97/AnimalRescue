6fd8ac67b6938bf3bec18a108e0b78bcec811618
Tahmid Muttaki <tahmid97@vt.edu>
2020-12-03 12:05:26 -0500

    added rescue groups api retrieval

--- AnimalRescue/AppDelegate.swift
+++ AnimalRescue/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         populateLists()
         readAnimalsDataFiles()
-        //print(catFacts)
+        //getAnimalRescueDataFromApi()
         return true
     }
     
---
+++ AnimalRescue/RescueGroupsApiData.swift
@@ -0,0 +1,142 @@
+//
+//  RescueGroupsApiData.swift
+//  AnimalRescue
+//
+//  Created by CS3714 on 12/3/20.
+//  Copyright © 2020 CS3714-91386 Team 16. All rights reserved.
+//
+
+import Foundation
+
+fileprivate let appKey = "GLJHM8fB"
+
+
+public func getAnimalRescueDataFromApi() {
+    /*
+     *********************************************
+     *   Obtaining API Search Query URL Struct   *
+     *********************************************
+     */
+    var apiQueryUrlStruct: URL?
+    
+    if let urlStruct = URL(string: "https://test1-api.rescuegroups.org/v5/public/animals/search/available/cats/haspic/?sort=random&limit=5") {
+        apiQueryUrlStruct = urlStruct
+    } else {
+        return
+    }
+    
+    /*
+     *******************************
+     *   HTTP GET Request Set Up   *
+     *******************************
+     */
+    
+    let headers = [
+        "Authorization": appKey,
+    ]
+    
+    let request = NSMutableURLRequest(url: apiQueryUrlStruct!,
+                                      cachePolicy: .useProtocolCachePolicy,
+                                      timeoutInterval: 10.0)
+    /*
+     You may need to increase the timeout interval from 10 seconds to a higher value
+     if fetching data from the API is much more time consuming.
+     */
+    request.httpMethod = "GET"
+    request.allHTTPHeaderFields = headers
+    
+    /*
+     *********************************************************************
+     *  Setting Up a URL Session to Fetch the JSON File from the API     *
+     *  in an Asynchronous Manner and Processing the Received JSON File  *
+     *********************************************************************
+     */
+    
+    /*
+     Create a semaphore to control getting and processing API data.
+     signal() -> Int    Signals (increments) a semaphore.
+     wait()             Waits for, or decrements, a semaphore.
+     */
+    let semaphore = DispatchSemaphore(value: 0)
+    
+    URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+        /*
+         URLSession is established and the JSON file from the API is set to be fetched
+         in an asynchronous manner. After the file is fetched, data, response, error
+         are returned as the input parameter values of this Completion Handler Closure.
+         */
+        
+        // Process input parameter 'error'
+        guard error == nil else {
+            semaphore.signal()
+            return
+        }
+        
+        // Process input parameter 'response'. HTTP response status codes from 200 to 299 indicate success.
+        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+            semaphore.signal()
+            return
+        }
+        
+        // Process input parameter 'data'. Unwrap Optional 'data' if it has a value.
+        guard let jsonDataFromApi = data else {
+            semaphore.signal()
+            return
+        }
+        
+        //------------------------------------------------
+        // JSON data is obtained from the API. Process it.
+        //------------------------------------------------
+        do {
+            /*
+             Foundation framework’s JSONSerialization class is used to convert JSON data
+             into Swift data types such as Dictionary, Array, String, Number, or Bool.
+             */
+            let jsonResponse = try JSONSerialization.jsonObject(with: jsonDataFromApi,
+                                                                options: JSONSerialization.ReadingOptions.mutableContainers)
+            
+            /*
+             JSON object with Attribute-Value pairs corresponds to Swift Dictionary type with
+             Key-Value pairs. Therefore, we use a Dictionary to represent a JSON object
+             where Dictionary Key type is String and Value type is Any (instance of any type)
+             */
+            var jsonDataDictionary = Dictionary<String, Any>()
+            
+            if let jsonObject = jsonResponse as? [String: Any] {
+                jsonDataDictionary = jsonObject
+            } else {
+                semaphore.signal()
+                return
+            }
+            
+            let meta = jsonDataDictionary["meta"] as! [String: Any]
+            let count = meta["count"] as! Int
+
+            print(jsonDataDictionary)
+            
+            
+        } catch {
+            semaphore.signal()
+            return
+        }
+        
+        semaphore.signal()
+    }).resume()
+    
+    /*
+     The URLSession task above is set up. It begins in a suspended state.
+     The resume() method starts processing the task in an execution thread.
+     
+     The semaphore.wait blocks the execution thread and starts waiting.
+     Upon completion of the task, the Completion Handler code is executed.
+     The waiting ends when .signal() fires or timeout period of 10 seconds expires.
+     */
+    
+    _ = semaphore.wait(timeout: .now() + 10)
+    
+    /*
+     You may need to increase the timeout interval from 10 seconds to a higher value
+     if fetching data from the API is much more time consuming.
+     */
+    
+}
